2944841382093cfed950875a3ba1e9bee9ecd2cf
Erdem Elvane <erdemelvan35@gmail.comm>
2023-03-20 19:06:51 +0300

    Loading spinner added

--- src/js/controller.js
+++ src/js/controller.js
@@ -14,9 +14,22 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
+function renderSpinner(parentEl) {
+  const markup = `
+  <div class="spinner">
+    <svg>
+      <use href="${icons}#icon-loader"></use>
+    </svg>
+  </div>       
+  `;
+  parentEl.innerHTML = '';
+  parentEl.insertAdjacentHTML('afterbegin', markup);
+}
+
 async function showRecipe() {
   try {
     // 1) Loading recipe
+    renderSpinner(recipeContainer)
     const res = await fetch(
       //'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcfb2'
       'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
