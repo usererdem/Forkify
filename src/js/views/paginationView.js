333a34d7f30738a17fdc1d3f38b1529d69e98822
Erdem Elvane <erdemelvan35@gmail.comm>
2023-04-03 04:07:53 +0300

    1) paginationView file: - added data-goto in markup - addHandlerClick func does select the button on click - put the new values for the page numbers in a variable - calls handler function with the variable 2) controller.js file: - addHandlerClick(controlPagination) calls the controlPagination with the variable that we receive from paginationView - render the getSearchResultPage with the received variable value - render page numbers

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -4,13 +4,25 @@ import icons from 'url:../../img/icons.svg';
 class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
 
+  addHandlerClick(handler) {
+    this._parentElement.addEventListener('click', function(e) {
+      const btn = e.target.closest('.btn--inline');
+      
+      if (!btn) return;
+
+      const goToPage = +btn.dataset.goto;
+      
+      handler(goToPage);
+    })
+  }
+
   _generateMarkup() {
     const curPage = this._data.page;
     const numPages = Math.ceil(
       this._data.results.length / this._data.resultsPerPage
     );
     const prevButtonMarkup = `
-    <button class="btn--inline pagination__btn--prev">
+    <button data-goto="${curPage - 1}" class="btn--inline pagination__btn--prev">
       <svg class="search__icon">
         <use href="${icons}#icon-arrow-left"></use>
       </svg>
@@ -18,7 +30,7 @@ class PaginationView extends View {
     </button>
     `;
     const nextButtonMarkup = `
-    <button class="btn--inline pagination__btn--next">
+    <button data-goto="${curPage + 1}" class="btn--inline pagination__btn--next">
       <span>Page ${curPage + 1}</span>
       <svg class="search__icon">
         <use href="${icons}#icon-arrow-right"></use>
