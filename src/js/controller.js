9a881a8d86ad1ebe3fa074942ee17d4f49bc9177
usererdem <erdemelvan35@gmail.com>
2023-04-12 17:23:13 +0300

    config.js: - API key added as variable - modal close seceond added as variable controller.js: controlAddRecipe func: - renders spinner on loading - calls uploadRecipe func - renders the uploaded recipe - closes modal success message after some time helpers.js: - sendJSON function added model.js: - createRecipeObject func added for reusability - if have api key adds key value in recipe object now - bugFix: typo in ingredients - recipe object created, sent as json format with sendJSON func put back in right format with createRecipeObject func - add it into bookmark addRecipeView.js: - Success message added

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,5 @@
 import * as model from './model.js';
+import { MODAL_CLOSE_SEC } from './config.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
@@ -96,9 +97,24 @@ function controlBookmarks() {
 
 async function controlAddRecipe(newRecipe) {
   try {
+    // Show loading spinner
+    addRecipeView.renderSpinner();
+    
     // Upload the new recipe data
     await model.uploadRecipe(newRecipe);
-  } catch(err) {
+    console.log(model.state.recipe);
+
+    // Render recipe
+    recipeView.render(model.state.recipe);
+
+    // Success message
+    addRecipeView.renderMessage();
+
+    // Close form window
+    setTimeout(function () {
+      addRecipeView.toggleWindow();
+    }, MODAL_CLOSE_SEC * 1000);
+  } catch (err) {
     console.error('❌❌❌', err);
     addRecipeView.renderError(err.message);
   }
