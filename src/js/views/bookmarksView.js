c5391a2867916b3618b2a92b621b77402598847a
Erdem Elvane <erdemelvan35@gmail.comm>
2023-04-12 14:38:47 +0300

    controller.js: - calling bookmarksView.render with bookmarks stored model.js: - added init func - gets item from localStorage and puts it in storage variable - if has item, JSON.parse it and save in state.bookmarks - JSON.parse it bookmarksView.js: - addHandlerRender added

--- src/js/views/bookmarksView.js
+++ src/js/views/bookmarksView.js
@@ -6,6 +6,10 @@ class BookmarksView extends PreviewView {
   _parentElement = document.querySelector('.bookmarks__list');
   _errorMessage = 'No bookmarks yet. Find a nice recipe and bookmark it :)';
   _message = '';
+
+  addHandlerRender(handler) {
+    window.addEventListener('load', handler);
+  }
 }
 
 export default new BookmarksView();
