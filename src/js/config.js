9033ccf5b8bbc2bca2548f8ff6331bf0654a019f
Erdem Elvane <erdemelvan35@gmail.comm>
2023-04-12 17:23:13 +0300

    config.js: - API key added as variable - modal close seceond added as variable controller.js: controlAddRecipe func: - renders spinner on loading - calls uploadRecipe func - renders the uploaded recipe - closes modal success message after some time helpers.js: - sendJSON function added model.js: - createRecipeObject func added for reusability - if have api key adds key value in recipe object now - bugFix: typo in ingredients - recipe object created, sent as json format with sendJSON func put back in right format with createRecipeObject func - add it into bookmark addRecipeView.js: - Success message added

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,6 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = 'a4f7a146-a701-4df2-a9af-9fae19234778';
+export const MODAL_CLOSE_SEC = 1.5;
+// https://forkify-api.herokuapp.com/v2
--- src/js/helpers.js
+++ src/js/helpers.js
@@ -11,7 +11,27 @@ const timeout = function (s) {
 
 export async function getJSON(url) {
   try {
-    const res = await Promise.race([fetch(url), timeout(TIMEOUT_SEC)]);
+    const fetchPro = fetch(url);
+    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    return data;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export async function sendJSON(url, uploadData) {
+  try {
+    const fetchPro = fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(uploadData),
+    });
+    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
     const data = await res.json();
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
